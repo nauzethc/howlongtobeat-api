f1786f4b7094ca0d8df1efc9b814c5a67efc0a38
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2021-11-12 08:52:29 +0000

    Code restructured to export types on index

--- lib/index.ts
+++ lib/index.ts
@@ -1,36 +1,30 @@
-import { Query, GameResult, GameDetail } from './types'
-import { parseGameDetail, parseGameResults, parseQuery } from './parsers'
-import axios from 'axios'
-import config from './howlongtobeat.json'
+import { find, get } from './howlongtobeat'
+import {
+  GameResult,
+  GameDetail,
+  Query,
+  LengthType,
+  SortBy,
+  SortOrder,
+  Modifier,
+  Perspective,
+  Flow,
+  Genre,
+  Platform
+} from './types'
 
-export async function find (
-  query: Query
-): Promise<{ total: number; data: GameResult[] }> {
-  return axios({
-    method: 'POST',
-    baseURL: config.BASE_URL,
-    url: config.SEARCH_ENDPOINT,
-    data: parseQuery(query),
-    params: { page: query.page || 1 },
-    timeout: 20000,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    transformResponse: parseGameResults
-  }).then(res => res.data)
+export {
+  find,
+  get,
+  GameResult,
+  GameDetail,
+  Query,
+  LengthType,
+  SortBy,
+  SortOrder,
+  Modifier,
+  Perspective,
+  Flow,
+  Genre,
+  Platform
 }
-
-export async function get (id: number): Promise<GameDetail | null> {
-  return axios({
-    method: 'GET',
-    baseURL: config.BASE_URL,
-    url: config.DETAIL_ENDPOINT,
-    params: { id },
-    timeout: 20000,
-    transformResponse: parseGameDetail
-  })
-    .then(res => res.data)
-    .catch(err => null)
-}
-
-export default { find, get }
